53f1f580cff3b41e03e9587f933de3f3a71e195d
csterebet <cristina_spac@yahoo.com>
2024-11-07 23:15:56 +0200

    Addedthetheimplementationimplementationof a of a librarylibrarysystemsystembybyusingusingthethecollectionscollections

---
+++ src/main/java/bookcollectiontask/Book.java
@@ -0,0 +1,19 @@
+package bookcollectiontask;
+
+public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
